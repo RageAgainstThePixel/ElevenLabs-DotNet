04d9862860b367b1e2dbd99eeae3e222b575aaeb
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-11 18:43:11 -0500

    ElevenLabs-DotNet 1.0.2
    - Added VoiceGenerationEndpoint
    - Added unit tests for voice design and instant voice cloning
    - Updated docs

--- ElevenLabs-DotNet/VoiceGeneration/GeneratedVoiceRequest.cs
+++ ElevenLabs-DotNet/VoiceGeneration/GeneratedVoiceRequest.cs
@@ -1,29 +1,66 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Text.Json.Serialization;
 
 namespace ElevenLabs.VoiceGeneration
 {
     public sealed class GeneratedVoiceRequest
     {
-        [JsonInclude]
+        /// <summary>
+        /// Voice Generation Request.
+        /// Use <see cref="VoiceGenerationEndpoint.GetVoiceGenerationOptionsAsync"/> to get a full list of options.
+        /// </summary>
+        /// <param name="text">Sample text to return for voice generation. Must be between 100 and 1000 characters.</param>
+        /// <param name="gender">The gender of the voice to generate.</param>
+        /// <param name="accent">The accent of the voice to generate.</param>
+        /// <param name="age">The age of the voice to generate.</param>
+        /// <param name="accentStrength">Optional, accept strength, between 0.3 - 2.</param>
+        public GeneratedVoiceRequest(string text, Gender gender, Accent accent, Age age, double accentStrength = 1)
+        {
+            switch (text.Length)
+            {
+                case < 100:
+                    throw new ArgumentOutOfRangeException(nameof(text), $"{nameof(text)} must be longer than 100 characters.");
+                case > 1000:
+                    throw new ArgumentOutOfRangeException(nameof(text), $"{nameof(text)} cannot be longer than 1000 characters.");
+                default:
+                    if (string.IsNullOrWhiteSpace(text))
+                    {
+                        throw new ArgumentNullException(nameof(text));
+                    }
+
+                    break;
+            }
+
+            Text = text;
+            Gender = gender.Code;
+            Accent = accent.Code;
+            Age = age.Code;
+
+            accentStrength = accentStrength switch
+            {
+                < 0.3f => 0.3f,
+                > 2f => 2f,
+                _ => accentStrength
+            };
+
+            AccentStrength = accentStrength;
+        }
+
         [JsonPropertyName("text")]
-        public string Text { get; private set; }
+        public string Text { get; }
 
-        [JsonInclude]
         [JsonPropertyName("gender")]
-        public string Gender { get; private set; }
+        public string Gender { get; }
 
-        [JsonInclude]
         [JsonPropertyName("accent")]
-        public string Accent { get; private set; }
+        public string Accent { get; }
 
-        [JsonInclude]
         [JsonPropertyName("age")]
-        public string Age { get; private set; }
+        public string Age { get; }
 
-        [JsonInclude]
         [JsonPropertyName("accent_strength")]
-        public int AccentStrength { get; private set; }
+        public double AccentStrength { get; }
     }
 }
