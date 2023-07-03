b64fd48afb56ef7d99f1ddd6b7fa837b79a96985
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2023-07-03 12:17:49 +0200

    Update VoiceSettings.cs
    added Style and use_speaker_boost
    Default values set via optional parameters.

--- ElevenLabs-DotNet/Voices/VoiceSettings.cs
+++ ElevenLabs-DotNet/Voices/VoiceSettings.cs
@@ -1,5 +1,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Text.Json.Serialization;
 
 namespace ElevenLabs.Voices
@@ -7,10 +8,12 @@ namespace ElevenLabs.Voices
     public sealed class VoiceSettings
     {
         [JsonConstructor]
-        public VoiceSettings(float stability, float similarityBoost)
+        public VoiceSettings(float stability, float similarityBoost, Boolean speakerboost=true, float style=0.45f)
         {
             Stability = stability;
             SimilarityBoost = similarityBoost;
+            Style = style;
+            SpeakerBoost = speakerboost;
         }
 
         [JsonPropertyName("stability")]
@@ -18,5 +21,9 @@ namespace ElevenLabs.Voices
 
         [JsonPropertyName("similarity_boost")]
         public float SimilarityBoost { get; set; }
+        [JsonPropertyName("style")]
+        public float Style { get; set; }
+        [JsonPropertyName("use_speaker_boost")]
+        public Boolean SpeakerBoost { get; set; }
     }
 }
