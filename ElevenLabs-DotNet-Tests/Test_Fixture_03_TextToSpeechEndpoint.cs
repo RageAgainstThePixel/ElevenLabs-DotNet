7db5d9b146328ca05a4008d855a671d068cf46aa
Ed Sparkes <1023491+ed-sparkes@users.noreply.github.com>
2023-11-15 20:51:29 +0000

    add unit test

--- ElevenLabs-DotNet-Tests/Test_Fixture_03_TextToSpeechEndpoint.cs
+++ ElevenLabs-DotNet-Tests/Test_Fixture_03_TextToSpeechEndpoint.cs
@@ -20,5 +20,26 @@ namespace ElevenLabs.Tests
             Assert.NotNull(clipPath);
             Console.WriteLine(clipPath);
         }
+
+        [Test]
+        public async Task Test_02_TextToSpeechPartial()
+        {
+            Assert.NotNull(ElevenLabsClient.TextToSpeechEndpoint);
+            var voice = (await ElevenLabsClient.VoicesEndpoint.GetAllVoicesAsync()).FirstOrDefault();
+            Assert.NotNull(voice);
+            bool callbackCalled = false;
+            var defaultVoiceSettings = await ElevenLabsClient.VoicesEndpoint.GetDefaultVoiceSettingsAsync();
+            var clipPath = await ElevenLabsClient.TextToSpeechEndpoint.TextToSpeechAsync("The quick brown fox jumps over the lazy dog.", voice, defaultVoiceSettings,
+            
+             partialClipCallback:  (partialClip) =>
+            {
+                callbackCalled = true;
+                return null;
+            });
+            
+            Assert.NotNull(clipPath);
+            Assert.IsTrue(callbackCalled);
+            Console.WriteLine(clipPath);
+        }
     }
 }
