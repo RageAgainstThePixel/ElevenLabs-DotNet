8b5f48364566b5f69f1c1688a315d9e5e86c9bd9
Austin Hale <haleau@gmail.com>
2024-07-05 10:29:38 -0700

    refactor: support both net6.0 & net8.0 refactor: spaces instead of tabs https://youtu.be/SsoOG6ZeyUI

--- ElevenLabs-DotNet-Tests/Test_Fixture_07_DubbingEndpoint.cs
+++ ElevenLabs-DotNet-Tests/Test_Fixture_07_DubbingEndpoint.cs
@@ -4,49 +4,48 @@ namespace ElevenLabs.Tests;
 using ElevenLabs.Dubbing;
 using NUnit.Framework;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
 internal class Test_Fixture_07_DubbingEndpoint : AbstractTestFixture
 {
-	[Test]
-	public async Task Test_01_Dubbing()
-	{
-		Assert.NotNull(ElevenLabsClient.DubbingEndpoint);
-		(string FilePath, string MediaType) audio = (Path.GetFullPath("../../../Assets/test_sample_01.ogg"), "audio/mpeg");
+    [Test]
+    public async Task Test_01_Dubbing()
+    {
+        Assert.NotNull(ElevenLabsClient.DubbingEndpoint);
+        (string FilePath, string MediaType) audio = (Path.GetFullPath("../../../Assets/test_sample_01.ogg"), "audio/mpeg");
 
-		FileInfo srcFile = new(audio.FilePath);
+        FileInfo srcFile = new(audio.FilePath);
 
-		DubbingRequest request = new()
-		{
-			File = audio,
-			SourceLanguage = "en",
-			TargetLanguage = "es",
-			NumSpeakers = 1,
-			Watermark = false,
-		};
+        DubbingRequest request = new()
+        {
+            File = audio,
+            SourceLanguage = "en",
+            TargetLanguage = "es",
+            NumSpeakers = 1,
+            Watermark = false,
+        };
 
-		(string DubbingId, float ExpectedDurationSecs) result = await ElevenLabsClient.DubbingEndpoint.DubbingAsync(request);
-		Assert.IsFalse(string.IsNullOrEmpty(result.DubbingId));
+        (string DubbingId, float ExpectedDurationSecs) result = await ElevenLabsClient.DubbingEndpoint.DubbingAsync(request);
+        Assert.IsFalse(string.IsNullOrEmpty(result.DubbingId));
 
-		Assert.IsTrue(await ElevenLabsClient.DubbingEndpoint.WaitForDubbingCompletionAsync(result.DubbingId, progress: new Progress<string>(msg => Console.WriteLine(msg))));
+        Assert.IsTrue(await ElevenLabsClient.DubbingEndpoint.WaitForDubbingCompletionAsync(result.DubbingId, progress: new Progress<string>(msg => Console.WriteLine(msg))));
 
-		FileInfo dubbedPath = new($"{srcFile.FullName}.dubbed.{request.TargetLanguage}{srcFile.Extension}");
-		{
-			await using FileStream fs = File.Open(dubbedPath.FullName, FileMode.Create);
-			await foreach (byte[] chunk in ElevenLabsClient.DubbingEndpoint.GetDubbedFileAsync(result.DubbingId, request.TargetLanguage))
-			{
-				await fs.WriteAsync(chunk);
-			}
-		}
-		Assert.IsTrue(dubbedPath.Exists);
+        FileInfo dubbedPath = new($"{srcFile.FullName}.dubbed.{request.TargetLanguage}{srcFile.Extension}");
+        {
+            await using FileStream fs = File.Open(dubbedPath.FullName, FileMode.Create);
+            await foreach (byte[] chunk in ElevenLabsClient.DubbingEndpoint.GetDubbedFileAsync(result.DubbingId, request.TargetLanguage))
+            {
+                await fs.WriteAsync(chunk);
+            }
+        }
+        Assert.IsTrue(dubbedPath.Exists);
 
-		FileInfo transcriptPath = new($"{srcFile.FullName}.dubbed.{request.TargetLanguage}.srt");
-		{
-			string transcriptFile = await ElevenLabsClient.DubbingEndpoint.GetTranscriptForDubAsync(result.DubbingId, request.TargetLanguage, "srt");
-			await File.WriteAllTextAsync(transcriptPath.FullName, transcriptFile);
-		}
-		Assert.IsTrue(transcriptPath.Exists);
-	}
+        FileInfo transcriptPath = new($"{srcFile.FullName}.dubbed.{request.TargetLanguage}.srt");
+        {
+            string transcriptFile = await ElevenLabsClient.DubbingEndpoint.GetTranscriptForDubAsync(result.DubbingId, request.TargetLanguage, "srt");
+            await File.WriteAllTextAsync(transcriptPath.FullName, transcriptFile);
+        }
+        Assert.IsTrue(transcriptPath.Exists);
+    }
 }
--- ElevenLabs-DotNet/Dubbing/DubbingRequest.cs
+++ ElevenLabs-DotNet/Dubbing/DubbingRequest.cs
@@ -2,84 +2,84 @@
 
 public sealed class DubbingRequest
 {
-	/// <summary>
-	/// automatic or manual. Manual mode is only supported when creating a dubbing studio project
-	/// </summary>
-	public string Mode { get; init; } = "automatic";
+    /// <summary>
+    /// automatic or manual. Manual mode is only supported when creating a dubbing studio project
+    /// </summary>
+    public string Mode { get; init; } = "automatic";
 
-	/// <summary>
-	/// A video (MediaType: "video/mp4") or audio (MediaType: "audio/mpeg") file intended for voice cloning
-	/// </summary>
-	public (string FilePath, string MediaType) File { get; init; }
+    /// <summary>
+    /// A video (MediaType: "video/mp4") or audio (MediaType: "audio/mpeg") file intended for voice cloning
+    /// </summary>
+    public (string FilePath, string MediaType) File { get; init; }
 
-	/// <summary>
-	/// CSV file containing transcription/translation metadata
-	/// </summary>
-	public string CsvFilePath { get; init; }
+    /// <summary>
+    /// CSV file containing transcription/translation metadata
+    /// </summary>
+    public string CsvFilePath { get; init; }
 
-	/// <summary>
-	/// For use only with csv input
-	/// </summary>
-	public string ForegroundAudioFilePath { get; init; }
+    /// <summary>
+    /// For use only with csv input
+    /// </summary>
+    public string ForegroundAudioFilePath { get; init; }
 
-	/// <summary>
-	/// For use only with csv input
-	/// </summary>
-	public string BackgroundAudioFilePath { get; init; }
+    /// <summary>
+    /// For use only with csv input
+    /// </summary>
+    public string BackgroundAudioFilePath { get; init; }
 
-	/// <summary>
-	/// Name of the dubbing project.
-	/// </summary>
-	public string Name { get; init; }
+    /// <summary>
+    /// Name of the dubbing project.
+    /// </summary>
+    public string Name { get; init; }
 
-	/// <summary>
-	/// URL of the source video/audio file.
-	/// </summary>
-	public string SourceUrl { get; init; }
+    /// <summary>
+    /// URL of the source video/audio file.
+    /// </summary>
+    public string SourceUrl { get; init; }
 
-	/// <summary>
-	/// Source language.
-	/// </summary>
-	/// <remarks>
-	/// A list of supported languages can be found at: https://elevenlabs.io/docs/api-reference/how-to-dub-a-video#list-of-supported-languages-for-dubbing
-	/// </remarks>
-	public string SourceLanguage { get; init; }
+    /// <summary>
+    /// Source language.
+    /// </summary>
+    /// <remarks>
+    /// A list of supported languages can be found at: https://elevenlabs.io/docs/api-reference/how-to-dub-a-video#list-of-supported-languages-for-dubbing
+    /// </remarks>
+    public string SourceLanguage { get; init; }
 
-	/// <summary>
-	/// The Target language to dub the content into. Can be none if dubbing studio editor is enabled and running manual mode
-	/// </summary>
-	/// <remarks>
-	/// A list of supported languages can be found at: https://elevenlabs.io/docs/api-reference/how-to-dub-a-video#list-of-supported-languages-for-dubbing
-	/// </remarks>
-	public string TargetLanguage { get; init; }
+    /// <summary>
+    /// The Target language to dub the content into. Can be none if dubbing studio editor is enabled and running manual mode
+    /// </summary>
+    /// <remarks>
+    /// A list of supported languages can be found at: https://elevenlabs.io/docs/api-reference/how-to-dub-a-video#list-of-supported-languages-for-dubbing
+    /// </remarks>
+    public string TargetLanguage { get; init; }
 
-	/// <summary>
-	/// Number of speakers to use for the dubbing. Set to 0 to automatically detect the number of speakers
-	/// </summary>
-	public int? NumSpeakers { get; init; }
+    /// <summary>
+    /// Number of speakers to use for the dubbing. Set to 0 to automatically detect the number of speakers
+    /// </summary>
+    public int? NumSpeakers { get; init; }
 
-	/// <summary>
-	/// Whether to apply watermark to the output video.
-	/// </summary>
-	public bool? Watermark { get; init; }
+    /// <summary>
+    /// Whether to apply watermark to the output video.
+    /// </summary>
+    public bool? Watermark { get; init; }
 
-	/// <summary>
-	/// Start time of the source video/audio file.
-	/// </summary>
-	public int? StartTime { get; init; }
+    /// <summary>
+    /// Start time of the source video/audio file.
+    /// </summary>
+    public int? StartTime { get; init; }
 
-	/// <summary>
-	/// End time of the source video/audio file.
-	/// </summary>
-	public int? EndTime { get; init; }
+    /// <summary>
+    /// End time of the source video/audio file.
+    /// </summary>
+    public int? EndTime { get; init; }
 
-	/// <summary>
-	/// Whether to use the highest resolution available.
-	/// </summary>
-	public bool? HighestResolution { get; init; }
+    /// <summary>
+    /// Whether to use the highest resolution available.
+    /// </summary>
+    public bool? HighestResolution { get; init; }
 
-	/// <summary>
-	/// Whether to prepare dub for edits in dubbing studio.
-	/// </summary>
-	public bool? DubbingStudio { get; init; }
+    /// <summary>
+    /// Whether to prepare dub for edits in dubbing studio.
+    /// </summary>
+    public bool? DubbingStudio { get; init; }
 }
