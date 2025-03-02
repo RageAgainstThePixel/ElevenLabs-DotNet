16c7df90c510bb00ed1423d3d751d26e1a53de80
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-03-01 23:15:29 -0500

    - Added VoiceSettings.ctr overload
      - Added speed property

--- ElevenLabs-DotNet/Voices/VoiceSettings.cs
+++ ElevenLabs-DotNet/Voices/VoiceSettings.cs
@@ -1,22 +1,35 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Text.Json.Serialization;
 
 namespace ElevenLabs.Voices
 {
     public sealed class VoiceSettings
     {
-        [JsonConstructor]
+        [Obsolete("use new .ctr overload")]
         public VoiceSettings(
             float stability = .75f,
             float similarityBoost = .75f,
             bool speakerBoost = true,
             float style = 0.45f)
+            : this(stability, similarityBoost, style, speakerBoost)
+        {
+        }
+
+        [JsonConstructor]
+        public VoiceSettings(
+            float stability = .75f,
+            float similarityBoost = .75f,
+            float style = 0.45f,
+            bool speakerBoost = true,
+            float speed = 1f)
         {
             Stability = stability;
             SimilarityBoost = similarityBoost;
             Style = style;
             SpeakerBoost = speakerBoost;
+            Speed = speed;
         }
 
         [JsonPropertyName("stability")]
@@ -30,5 +43,8 @@ namespace ElevenLabs.Voices
 
         [JsonPropertyName("use_speaker_boost")]
         public bool SpeakerBoost { get; set; }
+
+        [JsonPropertyName("speed")]
+        public float Speed { get; set; }
     }
 }
